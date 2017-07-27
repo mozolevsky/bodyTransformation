2f2a6bb1a6e0aae4ac37a53b88f1ed16df6eeb39
mozolevsky <alf.rulit@gmail.com>
2017-07-27 03:41:12 +0300

    All pages: fixed bugs from list 1. Calculator page: validation

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -6,10 +6,55 @@ let app = new Vue({
         formData: {
             formStepOne: true,
             weight: '',
+            weightUnitChecked: false,
             height: '',
+            heightUnitChecked: false,
             age: '',
             name: '',
             email: ''
+        }
+    },
+    methods: {
+        isNum(value) {
+            return !isNaN(value);
+        }
+    },
+    computed: {
+        weightUnit() {
+            return this.formData.weightUnitChecked ? 'kg' : 'lbs';
+        },
+        heightUnit() {
+            return this.formData.heightUnitChecked ? 'cm' : 'in';
+        },
+        weightStyles() {
+            return {
+              'calc-form__input_valid': this.formData.weight && this.isNum(this.formData.weight),
+              'calc-form__input_invalid': this.formData.weight && !this.isNum(this.formData.weight)
+            }
+        },
+        heightStyles() {
+            return {
+                'calc-form__input_valid': this.formData.height && this.isNum(this.formData.height),
+                'calc-form__input_invalid': this.formData.height && !this.isNum(this.formData.height)
+            }
+        },
+        ageStyles() {
+            return {
+                'calc-form__input_valid': this.formData.age && this.isNum(this.formData.age),
+                'calc-form__input_invalid': this.formData.age && !this.isNum(this.formData.age)
+            }
+        },
+        nameStyles() {
+            return {
+                'calc-form__input_valid': this.formData.name && this.isNum(this.formData.name),
+                'calc-form__input_invalid': this.formData.name && !this.isNum(this.formData.name)
+            }
+        },
+        emailStyles() {
+            return {
+                'calc-form__input_valid': this.formData.email && this.isNum(this.formData.email),
+                'calc-form__input_invalid': this.formData.email && !this.isNum(this.formData.email)
+            }
         }
     }
 });
